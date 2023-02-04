e069a6e7671c9013102317e92b8eaaefafc6e454
diego.ribeiro <diego.ribeiro@gazin.com.br>
2023-02-04 18:44:29 -0300

    fix: Importing CSS files

--- webpack.config.js
+++ webpack.config.js
@@ -36,11 +36,11 @@ module.exports = {
           //   },
         },
       },
-      //   {
-      //     test: /\.scss$/,
-      //     exclude: /node_modules/,
-      //     use: ["style-loader", "css-loader", "sass-loader"],
-      //   },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
